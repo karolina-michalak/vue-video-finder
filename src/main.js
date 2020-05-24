e8f1422950deb672ec084310495318b4d5144a80
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-24 12:21:41 +0200

    Another way of attachment DOM

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import App from "./App";
 
-
 //createElement = h
 
 new Vue({
-  render: h => h(App)
-});
+    render: (h) => h(App),
+  }).$mount('#app');
+  
+
+//lub:
+// new Vue({
+//     el: "#app",
+//     render: (h) => h(App),
+//   });
+  
+
