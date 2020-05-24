8a3cebaea48b32c521843e4ce1e3f5ce6a880027
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-24 14:02:40 +0200

    Created VideoList component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
-const API_KEY = 'AIzaSyAVccO5IEnvZ-VhsA8wfX7LRKfxKiww8IE'
+import VideoList from './components/VideoList';
+const API_KEY = "AIzaSyAVccO5IEnvZ-VhsA8wfX7LRKfxKiww8IE";
 
 export default {
   name: "App",
   components: {
     SearchBar,
+    VideoList
   },
   methods: {
-      onTermChange(searchTerm){
-          console.log(searchTerm)
-      }
-  }
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm,
+          },
+        })
+        .then((res) => console.log(res));
+    },
+  },
 };
 </script>
