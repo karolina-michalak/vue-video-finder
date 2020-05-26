433e5f7332ce5f61d537ebda74196da021ffcfcc
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-26 20:03:05 +0200

    Prop validation

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,16 @@
 <template>
     <ul>
         Video List
+        {{ videos }}
     </ul>
 </template>
 
 <script>
 export default {
-    name: "VideoList"
+    name: "VideoList",
+    props: {
+        videos: Array
+    }
 }
 </script>
 
