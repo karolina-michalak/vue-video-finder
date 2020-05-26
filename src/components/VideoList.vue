7a239a406d5ea4413dacb50ac1dd324a65106f6a
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-26 21:05:26 +0200

    Added styling and thumbnails

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,6 +1,7 @@
 <template>
-  <ul>
-    <VideoListItem v-for="video in videos" :key="video" v-bind:video="video">
+  <ul class="list-group">
+    <VideoListItem 
+    v-for="video in videos" :key="video.etag" :video="video">
     </VideoListItem>
   </ul>
 </template>
