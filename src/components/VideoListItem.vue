1f4bb0cacc051a6f566b0d60ee86e86b7d1d586f
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-27 21:32:10 +0200

    Styled list items

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,13 +1,29 @@
 <template>
-    <li class="list-group-item">
-        <img :src="video.snippet.thumbnails.default.url" />
-        {{ video.snippet.title }}
-    </li>
+  <li class="list-group-item media">
+    <img class="mr-3" :src="thumbnailUrl" />
+    <div class="media-body">{{ video.snippet.title }}</div>
+  </li>
 </template>
 
 <script>
 export default {
-    name: 'VideoListItem',
-    props: ['video']
-}
+  name: "VideoListItem",
+  props: ["video"],
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    },
+  },
+};
 </script>
+
+<style scoped>
+    li{
+        display: flex;
+        cursor: pointer;
+    }
+
+    li:hover{
+        background-color: #eee
+    }
+</style>
