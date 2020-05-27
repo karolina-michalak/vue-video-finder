28f72335c5ba09f092410ad0e3b4bb7a1047d0e6
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-27 22:16:49 +0200

    Passing props to video detail

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoDetail :video="selectedVideo" />
     <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
   </div>
 </template>
@@ -9,6 +10,7 @@
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
 import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
 const API_KEY = "AIzaSyAVccO5IEnvZ-VhsA8wfX7LRKfxKiww8IE";
 
 export default {
@@ -16,15 +18,16 @@ export default {
   components: {
     SearchBar,
     VideoList,
+    VideoDetail,
   },
   data() {
     return {
-      videos: [],
+      videos: [], selectedVideo: null
     };
   },
   methods: {
-    onVideoSelect(video){
-      console.log(video)
+    onVideoSelect(video) {
+      this.selectedVideo = video;
     },
     onTermChange(searchTerm) {
       axios
