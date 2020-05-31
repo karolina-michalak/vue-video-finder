efa169475f698f24ad0edbbfcb99da63b9293c78
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-31 11:43:59 +0200

    Styled video title

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,12 +1,24 @@
 <template>
   <div v-if="video">
-    {{ video.snippet.title }}
+    <div class="details">
+      <h4>{{ video.snippet.title }}</h4>
+      <p>{{ video.snippet.description }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "VideoDetail",
-  props: ['video']
+  props: ["video"],
 };
 </script>
+
+<style scoped>
+  .details{
+    margin-top: 10px;
+    padding: 10px;
+    border: 2px solid pink;
+    border-radius: 4px
+  }
+</style>
